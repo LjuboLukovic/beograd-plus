e4beecaf3567e1815cb21b879d0aaaeb51652ecd
Ljubo Lukovic <ljubolukovic93@gmail.com>
2023-05-29 23:50:10 +0200

    Added Firebase analytics to mobile apps

--- travel_app/lib/main.dart
+++ travel_app/lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 import 'package:beograd_plus/src/widgets/home/home_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+    if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  } else {
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
+  }
   runApp(const TravelApp());
 }
 
